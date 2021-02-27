f0f3e04f286f1ecb09f0c693f3f9369ed941c46a
Naufal Anargya <naufalanargyar@gmail.com>
2021-02-27 20:57:52 +0700

    Day2ARC

--- darkmode.js
+++ darkmode.js
@@ -1,4 +1,12 @@
-function myFunction() {
+function darkmode() {
     var element = document.body;
-    element.classList.toggle("dark-mode");
+    element.classList.toggle("darkmode");
+    var id = document.getElementById("darkmode_button")
+    if (id.src === "http://127.0.0.1:5500/icons/darkmode.jpg") { 
+    id.src = "http://127.0.0.1:5500/icons/lightmode.png"
+    } else {  
+      id.src = "http://127.0.0.1:5500/icons/darkmode.jpg"
+    }
+  console.log(id.src)
   }
+
